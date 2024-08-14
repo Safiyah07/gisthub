1a26167af53452de3b59e3dd86e088add102e030
Safiyah <safiyahmasud@gmail.com>
2024-08-14 11:43:12 +0100

    first page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Logo from "../../public/gih-logo.png";
 
 export default function Home() {
 	return (
 		<main className="m-10">
-			<p>The Gist</p>
+			<section className="text-center">
+				<div className="w-32 m-auto mb-4">
+					<Image
+						src={Logo}
+						alt="logo"
+					/>
+				</div>
+				<p>What gist did you come for today?</p>
+			</section>
 		</main>
 	);
 }
