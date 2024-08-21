b6246da29c2372d0e87732842908ccc47b93f0c7
Safiyah <safiyahmasud@gmail.com>
2024-08-21 18:45:08 +0100

    "Updated Latest.js component with new header and paragraph content"

--- src/components/aedc/Latest.js
+++ src/components/aedc/Latest.js
@@ -95,9 +95,26 @@ export default function Latest({ gist }) {
 
 	return (
 		<section className="flex flex-col gap-10 sm:gap-5">
-			<h1 className={`${palanquin.className} fade mt-5 text-xl`}>
-				Latest gist with AEDC
-			</h1>
+			<div>
+				<h1
+					className={`${palanquin.className} text-4xl font-semibold text-black capitalize dark:text-white md:text-3xl sm:text-2xl mt-5`}
+				>
+					Latest gist with AEDC
+				</h1>
+				<br />
+				{/* for waterboard page
+        Stay updated with the latest news from the Water Board, ensuring you never miss important information about water supply and distribution in your area. We provide timely updates on water rationing schedules, maintenance activities, and any service interruptions that might affect your daily routine. Our goal is to keep you well-informed about everything related to water services, so you can plan ahead and manage your resources effectively. Trust us to bring you reliable information directly from the Water Board, helping you stay prepared for any changes. */}
+				<p>
+					Stay connected with the latest updates from the Abuja Electricity
+					Distribution Company (AEDC). We bring you real-time information on
+					power supply schedules, planned outages, and emergency maintenance
+					activities across the region. Our updates ensure you&apos;re always
+					aware of any changes that might affect your electricity service,
+					helping you manage your energy needs efficiently. Whether it&apos;s a
+					new initiative from AEDC or important alerts on service disruptions,
+					we&apos;re here to keep you informed and empowered.
+				</p>
+			</div>
 
 			<div className="grid grid-cols-3 gap-10 sm:gap-5 md:grid-cols-2 sm:grid-cols-1">
 				{blogs.map((blog, id) => (
