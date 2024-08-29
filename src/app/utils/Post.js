237a8cd5e64588ff77ac20b68e6b8bee98703388
Safiyah <safiyahmasud@gmail.com>
2024-08-29 06:17:17 +0100

    contentful slug

---
+++ src/app/utils/Post.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Slug from "../aedc/[slug]/page";
+import { createClient } from "contentful";
+
+const client = createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function generateStaticParams() {
+	const response = await client.getEntries({
+		content_type: "gist",
+	});
+	const paths = response.items.map((post) => ({
+		params: { slug: post.fields.slug },
+	}));
+
+	return { paths, fallback: false };
+}
+
+export async function getBlog({ params }) {
+	try {
+		const { slug } = params;
+		const response = await client.getEntries({
+			content_type: "gist",
+			"fields.slug": slug,
+		});
+
+		if (!response.items) {
+			return "no gists";
+		} else {
+			return response.items;
+		}
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+}
+
+export default async function Post({ params }) {
+	const post = await getBlog(params);
+
+	console.log(post);
+	return (
+		<div>
+			<Slug />
+		</div>
+	);
+}
