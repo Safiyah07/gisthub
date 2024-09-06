50982b163d945183c77442a2ff59f5f8cf9e6a00
Safiyah <safiyahmasud@gmail.com>
2024-09-06 09:29:48 +0100

    contentful

--- src/app/aedc/[slug]/Post.js
+++ src/app/aedc/[slug]/Post.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Power1 from "../../../../public/power1.webp";
 import Power4 from "../../../../public/power4.webp";
 import Power5 from "../../../../public/power5.webp";
@@ -16,7 +17,7 @@ export default function Post({ gist }) {
 		thumbnail,
 		conclusionImage,
 		featuredImage,
-	} = gist;
+	} = gist[0].fields;
 	console.log("me me me me meme", gist);
 
 	return (
@@ -28,7 +29,8 @@ export default function Post({ gist }) {
 				<div className="flex gap-3 text-sm">
 					<p>From AEDC</p>
 					<p>-</p>
-					<p>20th August 2024</p>
+					{/* <p>20th August 2024</p> */}
+					<p>{time}</p>
 				</div>
 				<p>
 					Electricity bills in Nigeria have been a recurring source of
@@ -38,65 +40,27 @@ export default function Post({ gist }) {
 					explore some of the key factors contributing to the high electricity
 					bills in Nigeria.
 				</p>
-				{/* <Image
-					src={`https:${thumbnail.fields.file.url}`}
-					width={thumbnail.fields.file.details.image.width}
-					height={thumbnail.fields.file.details.image.height}
-					alt="thumbnail"
-					className="object-cover h-[350px] rounded-md"
-					priority
-				/> */}
-				<p>{info}</p>
-				<p>
-					1. Aging Infrastructure: One of the primary reasons for high
-					electricity bills is the aging and dilapidated state of Nigeria&apos;s
-					electricity infrastructure. The country&apos;s power grid has been
-					neglected for decades, leading to frequent power outages, transmission
-					losses, and inefficiencies. The cost of maintaining and upgrading this
-					infrastructure is passed on to consumers in the form of higher
-					tariffs.
-				</p>
-				<p>
-					2. Inadequate Supply: The demand for electricity in Nigeria far
-					exceeds the supply. This shortage has led to rationing and power
-					outages, forcing consumers to rely on alternative power sources like
-					generators, which can be expensive to operate. The cost of generating
-					electricity through these alternative means is often factored into the
-					overall electricity tariff.
-				</p>
-				<p>
-					3. Government Policies: Government policies and regulations play a
-					significant role in determining electricity tariffs. Subsidies and
-					price controls can distort the market and lead to inefficiencies.
-					Additionally, the cost of fuel for power generation, which is often
-					subsidized by the government, can also contribute to higher tariffs.
-				</p>
-				<p>
-					4. Corruption and Theft: Corruption and theft within the electricity
-					sector are pervasive problems in Nigeria. These issues can lead to
-					revenue losses, inefficiencies, and increased costs for consumers.
-				</p>
-				<p>
-					5. Distribution Losses: The transmission and distribution of
-					electricity in Nigeria is often inefficient, leading to significant
-					losses. These losses are ultimately borne by consumers in the form of
-					higher tariffs.
-				</p>
-				<p>
-					6. Lack of Investment: The Nigerian electricity sector has suffered
-					from a lack of investment for many years. This has hindered the
-					development of new power plants and the modernization of the existing
-					infrastructure. Without adequate investment, it is difficult to meet
-					the growing demand for electricity and reduce costs.
-				</p>
-				{/* <Image
-					src={`https:${featuredImage.fields.file.url}`}
-					width={featuredImage.fields.file.details.image.width}
-					height={featuredImage.fields.file.details.image.height}
-					alt="featured image"
-					className="object-cover h-[350px] rounded-md"
-					priority
-				/> */}
+				{thumbnail && (
+					<Image
+						src={`https:${thumbnail.fields.file.url}`}
+						width={thumbnail.fields.file.details.image.width}
+						height={thumbnail.fields.file.details.image.height}
+						alt="thumbnail"
+						className="object-cover h-[350px] w-auto shadow-[-1px_1px_4px_0_rgba(0,0,0,0.5)] rounded-md"
+						priority
+					/>
+				)}
+				<p>{documentToReactComponents(info)}</p>
+				{featuredImage && (
+					<Image
+						src={`https:${featuredImage.fields.file.url}`}
+						width={featuredImage.fields.file.details.image.width}
+						height={featuredImage.fields.file.details.image.height}
+						alt="featured image"
+						className="object-cover h-[350px] w-auto shadow-[-1px_1px_4px_0_rgba(0,0,0,0.5)] rounded-md"
+						priority
+					/>
+				)}
 				<p>
 					{" "}
 					Conclusion: The high cost of electricity in Nigeria is a complex issue
@@ -107,15 +71,40 @@ export default function Post({ gist }) {
 					Nigeria can work towards a more affordable and reliable electricity
 					supply for its citizens and businesses.
 				</p>
-				{/* <Image
-					src={`https:${conclusionImage.fields.file.url}`}
-					width={conclusionImage.fields.file.details.image.width}
-					height={conclusionImage.fields.file.details.image.height}
-					alt="image4"
-					className="object-cover h-[350px] rounded-md"
-					priority
-				/> */}
+				{conclusionImage && (
+					<Image
+						src={`https:${conclusionImage.fields.file.url}`}
+						width={conclusionImage.fields.file.details.image.width}
+						height={conclusionImage.fields.file.details.image.height}
+						alt="image4"
+						className="object-cover h-[350px] w-auto shadow-[-1px_1px_4px_0_rgba(0,0,0,0.5)] rounded-md"
+						priority
+					/>
+				)}
 			</div>
+
+			{/* Comment */}
+			<section className="w-full py-10">
+				<h1>Comments 2</h1>
+				<br />
+				<textarea
+					name="comments"
+					id="comments"
+					cols="30"
+					rows="10"
+					placeholder="Add Comments Here"
+					className="shadow-[-1px_1px_4px_0_rgba(0,0,0,0.5)] rounded-md w-full p-5"
+				></textarea>
+
+				<div className="flex items-center gap-2">
+					<div className="w-10 h-10">image</div>
+					<div>
+						<h4>Name</h4>
+						<p>comment</p>
+					</div>
+				</div>
+				{/* {comments && (<div></div>)} */}
+			</section>
 		</section>
 	);
 }
