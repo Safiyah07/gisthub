00a728f3b97edb757a550433b59a5a6b6cbea18f
Safiyah <safiyahmasud@gmail.com>
2024-09-13 09:07:30 +0100

    animation landing page

--- src/app/page.js
+++ src/app/page.js
@@ -16,6 +16,10 @@ export default function Home() {
 		// 	ease: "back.out",
 		// 	// delay: 0.5,
 		// 	// Optional delay before the animation starts
+		// delay: 0.5,
+		// ease: "back.out",
+		// repeat: -1,
+		// yoyo: true,
 		// });
 		gsap.fromTo(
 			".fade",
@@ -25,20 +29,20 @@ export default function Home() {
 				opacity: 1,
 				duration: 0.7,
 				stagger: 0.5,
-				// delay: 0.5,
-				// ease: "back.out",
-				// repeat: -1,
-				// yoyo: true,
 			}
 		);
-		gsap.fromTo(
-			".image",
-			{ opacity: 0 },
+
+		gsap.to(
+			".imageText",
+			// { hover: { opacity: 0, duration: 0, y: 40 } },
 			{
-				opacity: 1,
-				duration: 0.7,
-				delay: 0.4,
+				y: 50,
+				duration: 0.5,
 			}
+			// delay: 0.5,
+			// ease: "back.out",
+			// repeat: -1,
+			// yoyo: true,
 		);
 	}, []);
 
@@ -49,7 +53,7 @@ export default function Home() {
 				<h1 className={`${palanquin.className} fade lg:text-[2rem] mb-5`}>
 					What&apos;s the scoop you&apos;re after today?
 				</h1>
-				<p className="w-3/4 m-auto fade text-grey sm:w-full">
+				<p className="w-3/4 m-auto fade text-grey sm:w-full sm:text-lg">
 					We’re dishing out the hottest updates from AEDC and Water Board —
 					don’t blink!
 				</p>
@@ -57,7 +61,7 @@ export default function Home() {
 
 			{/* images */}
 			<div className="flex flex-col justify-between gap-10 lg:flex-row">
-				<div className="relative w-full lg:w-[49%] shadow-3xl rounded-lg transition-all ease-in-out duration-700 image">
+				<div className="relative w-full lg:w-[49%] shadow-3xl rounded-lg transition-all ease-in-out duration-700">
 					<div className="rounded-md h-[300px] bg-power1 bg-cover"></div>
 					<div className="absolute inset-0 bg-black rounded-md opacity-40"></div>
 					<Link
@@ -65,10 +69,10 @@ export default function Home() {
 						style={{ textShadow: "3px 1px 5px black" }}
 						className="absolute inset-0 flex items-center justify-center text-5xl font-medium text-white lg:text-7xl"
 					>
-						Power (AEDC)
+						<p className="imageText fade">Power (AEDC)</p>
 					</Link>
 				</div>
-				<div className="relative w-full lg:w-[49%] shadow-3xl rounded-lg transition-all ease-in-out duration-700 image">
+				<div className="relative w-full lg:w-[49%] shadow-3xl rounded-lg transition-all ease-in-out duration-700">
 					<div className="rounded-md h-[300px] bg-water1 bg-cover "></div>
 					<div className="absolute inset-0 bg-black rounded-md opacity-40"></div>
 					<Link
@@ -76,7 +80,7 @@ export default function Home() {
 						style={{ textShadow: "3px 1px 5px black" }}
 						className="absolute inset-0 flex items-center justify-center text-5xl font-medium text-white lg:text-7xl"
 					>
-						Water Board
+						<p className="imageText fade">Water Board</p>
 					</Link>
 				</div>
 			</div>
